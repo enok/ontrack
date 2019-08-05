b1b5bf576e8a92e7612c40329aff09e8833d8bce
Enok Antônio <enok.jesus-sysmap@claro.com.br>
2019-08-05 11:24:10 -0300

    Initial commit

---
+++ src/test/java/com/accounting/personal/ontrack/OntrackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.accounting.personal.ontrack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OntrackApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
